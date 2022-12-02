a9510cce1a123e0b244f5f1fed7abbc01f6fa1b1
agus08 <3103120119@student.skmktelkom-pwt.sch.id>
2022-12-02 13:44:44 +0700

    finish

---
+++ app/Models/Konser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Konser extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'tanggal',
+        'tempat',
+        'harga',
+        'deskripsi',
+        'poster',
+    ];
+
+    public function transaksi()
+    {
+        return $this->hasMany(Transaksi::class);
+    }
+}
